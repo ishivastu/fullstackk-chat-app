56a3b25295e7e15d23f7b13997bebec1082a3b3c
Shivastu Mishra <shivastumishra@gmail.com>
2026-04-26 22:31:21 +0530

    fix correct frontend path

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,10 +10,10 @@ const PORT = process.env.PORT || 5001;
 const __dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.use((req, res) => {
-    res.sendFile(path.join(__dirname, "frontend/dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
   });
 }
 
