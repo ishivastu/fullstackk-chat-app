0330dad69bb807c4a4266ddbe5ce3564c680610a
Shivastu Mishra <shivastumishra@gmail.com>
2026-06-13 19:57:55 +0530

    v67

--- backend/src/lib/generateToken.js
+++ backend/src/lib/generateToken.js
@@ -1,13 +1,28 @@
-import jwt from "jsonwebtoken";
+export const generateToken = (
+  userId,
+  res
+) => {
+  const token = jwt.sign(
+    { userId },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "7d",
+    }
+  );
 
-export const generateToken = (userId, res) => {
-  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-    expiresIn: "7d",
+  res.cookie("jwt", token, {
+    maxAge:
+      7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+    sameSite:
+      process.env.NODE_ENV ===
+      "production"
+        ? "none"
+        : "lax",
+    secure:
+      process.env.NODE_ENV ===
+      "production",
   });
 
-res.cookie("jwt", token, {
-  maxAge: 7 * 24 * 60 * 60 * 1000,
-  httpOnly: true,
-  sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-  secure: process.env.NODE_ENV === "production",
-})};
+  return token; // IMPORTANT
+};
