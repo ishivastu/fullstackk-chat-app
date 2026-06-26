18ceb401d6908651cecb96e24fa3748c085b67d4
Shivastu Mishra <shivastumishra@gmail.com>
2026-06-26 19:00:35 +0530

    new

--- backend/src/lib/redis.js
+++ backend/src/lib/redis.js
@@ -2,9 +2,9 @@ import { Redis } from "@upstash/redis";
 import { Ratelimit } from "@upstash/ratelimit";
 
 const redis = new Redis({
-  url: 'process.env.UPSTASH_REDIS_REST_URL',
-  token: 'process.env.UPSTASH_REDIS_REST_TOKEN',
-})
+  url: process.env.UPSTASH_REDIS_REST_URL,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN,
+});
 
 export const ratelimit = new Ratelimit({
   redis,
