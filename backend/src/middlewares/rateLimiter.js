529a962cac013c4b0ea0aa9ce1159c65f764574e
Shivastu Mishra <shivastumishra@gmail.com>
2026-06-26 17:34:00 +0530

    hgot

--- backend/src/middlewares/rateLimiter.js
+++ backend/src/middlewares/rateLimiter.js
@@ -1,12 +1,15 @@
-import rateLimit from 'express-rate-limit';
+import rateLimit from "express-rate-limit";
 
 const rateLimiter = rateLimit({
-  windowMs: 1 * 60 * 1000, // 1 minute
-  max: 5, // Limit each IP to 5 requests per `window` (here, per 1 minute)
+  windowMs: 15 * 60 * 1000,
+  max: 5,
 
-  message: {
-    status: 429,
-    message: 'Too many requests from this IP, please try again after a minute',
+  handler: (req, res) => {
+    console.log("Rate limit exceeded for:", req.ip);
+
+    res.status(429).json({
+      message: "Too many requests",
+    });
   },
 });
 
